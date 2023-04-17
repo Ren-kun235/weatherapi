3e22ed0c3c4395187e8de68875225e4d8b58930c
Ren-Kun235 <changpobzeb651@gmail.com>
2023-04-16 21:25:55 -0500

    weatherapi added to Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,3 @@
 var weatherAPIKey = "a1139edec8d600d35d1e71733cd412a8";
-var weatherUrl = "";
+var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherAPIKey;
+var city = "";
