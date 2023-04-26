7dc4ed704cb68d3be3a7e8695bbae0ac0bf537c5
Ren-Kun235 <changpobzeb651@gmail.com>
2023-04-25 19:59:22 -0500

    function is working, adding El's for dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,28 @@
 var weatherAPIKey = "a1139edec8d600d35d1e71733cd412a8";
-// var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherAPIKey;
-var city = "";
 
-var getReviews = function (weather) {
-    var weatherUrl = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": "f48dd780damshf6554dc854efa2fp1b25dfjsn85a9e44222d9",
-        "X-RapidAPI-Host": "opencritic-api.p.rapidapi.com",
-      },
-    };
-  
+var barEl = document.querySelector("#searchCity");
+var searchEl = document.querySelector("#button");
 
-  };
+searchEl.addEventListener("click", getApi);
 
   function getApi() {
-    var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherAPIKey;
+    var city = barEl.value;
+    var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + weatherAPIKey;
 
     fetch(weatherUrl)
       .then((response) => response.json())
-      .then((response) => console.log(response))
+      .then((res) => {
+        console.log(res)
+
+        var forecastEl = document.getElementById("ul");
+
+        for (i = 0; i < 5; i++) {
+          var cityEl = $("h3").addClass("weather");
+          var weatherEl = $("div").attr();
+          var tempEl = $("div").text(res.list[i*8].main.temp);
+        }
+
+      })
       .catch((err) => console.error(err));
-  }
+  };
+
