582c378437faf494807e5b8b24389402b666295e
Ren-Kun235 <changpobzeb651@gmail.com>
2023-05-28 13:42:17 -0500

    temp, icon, humidity, speeds, and date are visible in application

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,20 +12,39 @@ searchEl.addEventListener("click", getApi);
     fetch(weatherUrl)
       .then((response) => response.json())
       .then((res) => {
-
+        console.log(res);
         var cityName = res.city.name;
 
-        var cityNameEl = document.querySelector("#todaysForecast");
+        var cityNameEl = document.querySelector("#cityName");
         cityNameEl.textContent = cityName;
 
-        var searchHistory = res.city.history;
+        // var searchHistory = res.city.history;
 
-        var cityHistoryEl = document.querySelector(".history");
-        cityHistoryEl.textContent = searchHistory;
-        console.log(searchHistory);
+        // var cityHistoryEl = document.querySelector(".history");
+        // cityHistoryEl.textContent = searchHistory;
+        
+        for (let i = 0; i < 5; i++) {
 
-        // var displayWeatherEl = 
-        // displayWeatherEl.
+          var temp = res.list[i].main.temp;
+          var tempEl = document.querySelector(`#temp${i+1}`)
+          tempEl.textContent = temp;
+
+          var weatherIcon = res.list[i].weather[0].icon;
+          var weatherIconEl = document.querySelector(`#weatherIcon${i+1}`)
+          weatherIconEl.textContent = weatherIcon;
+
+          var humidity = res.list[i].main.humidity;
+          var humidityEl = document.querySelector(`#humidity${i+1}`)
+          humidityEl.textContent = humidity;
+
+          var windSpeeds = res.list[i].wind.speed;
+          var windSpeedsEl = document.querySelector(`#windSpeeds${i+1}`)
+          windSpeedsEl.textContent = windSpeeds;
+
+          var date = res.list[i].dt_txt;
+          var dateEl = document.querySelector(`#date${i+1}`)
+          dateEl.textContent = date;
+        }
 
       })
       .catch((err) => console.error(err));
